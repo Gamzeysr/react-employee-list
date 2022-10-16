e086584f1bd610005badd835f3d7253df07b375a
Gamze <gamzeyasar35@icloud.com>
2022-10-16 20:38:18 +0300

    first commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Card.css"
+
+
+const Card = ({ id, name, age, image, email }) => {
+    return (
+        <div className='Card'>
+            <img src={image} alt="img" className="main-img" />
+            <div className="name-info">
+                <h4>{name}</h4>
+                <p className="email">{email}</p>
+                <p className="age">{age} years old</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = ({ from, to }) => {
+    return (
+        <div className='header'>
+            <h1>Employee List</h1>
+            <h4>(Employes {from} to {to} to)</h4>
+
+        </div >
+    )
+}
+
+export default Header
